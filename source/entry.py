9e57081367093c79589d172b09e405f1848dfd6e
Robert Smallshire <rob@sixty-north.com>
2020-12-30 09:52:19 +0100

    Setup the system stack in page 0x02.

--- source/entry.py
+++ source/entry.py
@@ -1,5 +1,8 @@
 from asm68.asmdsl import AsmDsl
-from asm68.mnemonics import (FDB, ORG, NOP, JMP, LDA, STA, BITA, BEQ, CALL)
+from asm68.mnemonics import (
+    FDB, ORG, NOP, JMP, LDA, STA, BITA, BEQ, CALL, LDS, JSR, RTS, RTI,
+    LDMD
+)
 
 asm = AsmDsl()
 
@@ -26,10 +29,37 @@ def pad_until(address):
 #  picocom /dev/tty.usbserial-AI02KM9Y -b2400 -ye -d7 -p1
 
 
+# Memory Map
+# 0x00__ Zero page
+
+# 0x01__ System stack
+
+# The 6809 Stack grows downward (toward lower addresses); the Stack Pointer always contains the
+# address of the last occupied location, rather than the next empty one as on some other
+# microprocessors (including the 6800 and 6502). This means you must initialize the Stack Pointer
+# to a value one higher than the largest address in the Stack area (e.g., initializing the Stack
+# Pointer to 0100 16 means that the largest address in the Stack area will be 00FF ).
+
+system_stack_page = 0x01
+system_stack_size = 0xFF
+system_stack_base = (system_stack_page << 8) + system_stack_size + 1
+
+# 0x02__ OS workspace
+os_work_page = 0x20
+acia_control_register_copy = (os_work_page << 8) + 0x50
+
+
+aciacr = 0xA000  # 6850 control register
+aciasr = 0xA000
+aciadr = 0xA001
+
+
 asm         (   ORG,    0xC000,         "Bottom of the top 16 K ROM"    )
 
 asm .BEGIN  (   NOP,                    "Do nothing"                    )
 asm         (   NOP,                    "Do nothing"                    )
+asm .BEGIN  (   NOP,                    "Do nothing"                    )
+asm         (   LDS,    system_stack_base, "Setup system stack"         )
 asm         (   NOP,                    "Do nothing"                    )
 asm         (   LDA,    0b00000011,     "Master reset ACIA"             )
 asm         (   STA,    {aciacr}                                        )
@@ -51,7 +81,7 @@ asm         (   FDB,   (0xC000,     # Reserved
                         0xC000,     # SWI3
                         0xC000,     # SWI2
                         0xC000,     # /FIRQ
-                        0xC000,     # /IRQ
+                        asm.IRQ,    # /IRQ
                         0xC000,     # SWI
                         0xC000,     # /NMI
                         0xC000)     # /RESET
